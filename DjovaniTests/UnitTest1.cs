a285b1043a6625a4e472cb3e78f2400b844a362a
I-Milanov <i.t.milanov1996@gmail.com>
2025-03-18 13:09:27 +0200

    Create project

---
+++ DjovaniTests/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿// 1. Create Nunit Project
+// 2. Add Selenium nuget Packages (Web Driveer + Chrome driver)
+namespace DjovaniTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
